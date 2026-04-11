775fc69470d6168e643bd59a8e26e829378d9280
jpauloct-hash <jpauloct@gmail.com>
2026-04-11 00:10:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,9 @@
-// Self-destructing service worker - clears all caches
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
+self.addEventListener('install', function(e) { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
       return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-    }).then(function() {
-      return self.clients.claim();
-    })
+    }).then(function() { return self.clients.claim(); })
   );
 });
-self.addEventListener('fetch', function(e) {
-  // Never cache - always go to network
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('fetch', function(e) { e.respondWith(fetch(e.request)); });
